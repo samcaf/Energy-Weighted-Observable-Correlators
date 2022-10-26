8207588910bc202ee4efbc74744b5e3134f0110d
Samuel Alipour-fard <samuelaf@mit.edu>
2022-10-26 18:29:13 -0400

    Removed plot-specific scripts -- now redundant when using additional options added for the scripts in the ```scripts/``` folder.

--- plot_tools/scripts/qcd_Zpt.sh
+++
@@ -1,1 +0,0 @@
-python3 -m plot_tools/scripts/plot_ewocs_subrads.py -n 10000 -l parton -p qcd --plot_pt_pid 23
--- plot_tools/scripts/subrads_top_parton.sh
+++
@@ -1,1 +0,0 @@
-python3 plot_tools/scripts/plot_ewocs_subrads.py -n 10000 -l parton -p top -j antikt -R 1.0 -s ca -r [0.0,0.10,.2,.3,.4,.5] --pt_min 50 --pt_max 3000 -E 10000 --s_channel gmZ
